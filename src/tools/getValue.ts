b213f32461c2fe22c033c1b7ebfd79ccc6753349
Seonghwa Yun <seonghwa.yun@comcom.ai>
2025-04-23 15:21:53 +0900

    fix wrong endpoint, update return value of tool

--- src/tools/getValue.ts
+++ src/tools/getValue.ts
@@ -12,22 +12,29 @@ export const getValue: ToolType<typeof parameters> = {
   description: 'Get the value of a path from the database on AI Network',
   parameters,
   handler: async (params: z.infer<typeof parameters>, context: ToolContext) => {
-    const res = await fetch(`${MCP_SERVER_CONFIG.providerUrl}/json_rpc`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: 'ain_get',
-        params: {
-          "protoVer": "1.5.1",
-          "type": "GET_VALUE",
-          "ref": params.path
-        }
-      })
-    });
-    const data = await res.json();
-    return data;
+    try {
+      const res = await fetch(`${MCP_SERVER_CONFIG.providerUrl}/json-rpc`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "jsonrpc": "2.0",
+          "id": 1,
+          "method": "ain_get",
+          "params": {
+            "protoVer": "1.5.1",
+            "type": "GET_VALUE",
+            "ref": params.path
+          }
+        })
+      });
+      const data = await res.json();
+      return {
+        content: [{ type: 'text', text: JSON.stringify(data.result.result) }],
+      };
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
