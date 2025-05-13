d6fd8e7a01eb072989265699b4ce0b18374e8727
Seonghwa Yun <seonghwa.yun@comcom.ai>
2025-05-13 17:21:15 +0900

    update with ain-js

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,7 +1,9 @@
 import { ToolType } from "../types";
 import { getValue } from "./getValue";
+import { setValue } from "./setValue";
 
 export const Tools: Array<ToolType<any>> = [
   getValue,
+  setValue,
   // Add more tools here
 ];
